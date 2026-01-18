f319dd7f2e94a0197b69b5c0906f0af19900e8d0
Chilling Corn <phamhuy050407@gmail.com>
2026-01-18 12:02:06 +0700

    Temporary adding HSG/2013/1 - Route.cpp

---
+++ vnoj/HSG12/2013/1 - Route.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    freopen("ROUTE.INP","r",stdin);
+    freopen("ROUTE.OUT","w",stdout);
+    int n,m;
+    std::cin>>n>>m;
+    while (m--){
+        std::cin>>
+    }
+}
